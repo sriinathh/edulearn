26a4510df9dbfed0f212569f2302cf0503c4de59
Srinath <potharajusrinathh@gmail.com>
2025-05-19 15:43:32 +0530

    remove error 1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  logLevel: 'error'  // <-- Add this line to hide warnings and show only errors
-});
+  build: {
+    rollupOptions: {
+      external: [],  // We'll add module names here if needed
+    },
+  },
+})

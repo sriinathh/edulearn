f8760ace6287c77f5d286a3349d4cac61889b8b7
Srinath <potharajusrinathh@gmail.com>
2025-05-19 14:30:58 +0530

    ready for deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  logLevel: 'error'  // <-- Add this line to hide warnings and show only errors
 });
